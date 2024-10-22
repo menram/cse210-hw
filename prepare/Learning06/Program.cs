a8353706dabfe3bed84a5c7ffbe6ff28dd87bc14
menram <men24030@byui.edu>
2024-10-21 20:01:03 -0700

    Polymorphism

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        // Console.WriteLine("Hello Learning06 World!");
+        List<Shape> shapeList = [];
+        Square square = new("red", 4);
+        shapeList.Add(square);
+        Rectangle rectangle = new("blue", 2, 6);
+        shapeList.Add(rectangle);
+        Circle circle = new("yellow", 2);
+        shapeList.Add(circle);
+
+        foreach (Shape s in shapeList)
+        {
+            Console.WriteLine($"Shape Color: {s.GetColor()} Shape Area: {s.GetArea()}");
+        }
     }
 }
